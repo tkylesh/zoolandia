d13e0749ae0166e393f452bd49676e479411b4a1
Taylor Shuff <tkylesh@gmail.com>
2017-01-16 10:48:47 -0600

    added animals class.

---
+++ Animals.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Zoolandia.Animals
+{
+    class Animal
+    {
+        public string name { get; set; }
+        public int arms {get; set;}
+        public int legs {get; set;}
+
+        public bool fur {get; set;}
+
+
+        public void sleep ()
+        {
+            Console.WriteLine("ZZzzzzzzz...");
+        }
+    }
+
+    // The species for a Red Pana
+    class AilurusFulgens : Animal
+    {
+        // Define simple properties for a Red Panda
+        public string eats {get; set;}
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Zoolandia.Animals;
 
 namespace ConsoleApplication
 {
