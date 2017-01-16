870c61645eac5d574b41ba5735a2ff8d752b144e
Taylor Shuff <tkylesh@gmail.com>
2017-01-16 15:30:31 -0600

    instantiated instance of red panda and console logged property to console.

--- Program.cs
+++ Program.cs
@@ -12,7 +12,9 @@ namespace ConsoleApplication
             //Instance of red panda class
             var redPanda = new AilurusFulgens();
             redPanda.eatsBamboo = true;
-            Console.WriteLine("(true/false) Does a red panda eat bamboo? "+ redPanda.eatsBamboo);
+            Console.WriteLine("(true/false) Does a red panda eat bamboo? "+ redPanda.eatsBamboo + "\n");
+            redPanda.name = "Mr. panda red";
+            Console.WriteLine("panda's name is "+ redPanda.name + "\n");
         }
     }
 }
